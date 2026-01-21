75eb80a13532467668b936ac9874df66e049e081
Skyshi Digital Indonesia <skyshidigitalindonesia@Skyshis-MacBook-Pro.local>
2026-01-21 09:58:25 +0700

    Update add debugprint

--- lib/shared/services/content_recorder/services/image_render_service.dart
+++ lib/shared/services/content_recorder/services/image_render_service.dart
@@ -79,11 +79,14 @@ class ImageRenderService {
         useThumbnailSize,
       );
 
+      debugPrint('isOutputSizeTooLarge: $isOutputSizeTooLarge');
+
       if (isOutputSizeTooLarge) {
         outputRatio = max(
           _maxOutputDimension(useThumbnailSize).width / boundary.size.width,
           _maxOutputDimension(useThumbnailSize).height / boundary.size.height,
         );
+        debugPrint('outputRatio: $outputRatio');
       }
 
       double pixelRatio = configs.customPixelRatio ?? outputRatio;
@@ -118,7 +121,8 @@ class ImageRenderService {
     bool useThumbnailSize,
   ) {
     Size outputSize = renderedSize * outputRatio;
-
+    debugPrint('outputSize width: ${outputSize.width}, height: ${outputSize.height}');
+    debugPrint('maxOutputDimension width: ${_maxOutputDimension(useThumbnailSize).width}, height: ${_maxOutputDimension(useThumbnailSize).height}');
     return outputSize.width > _maxOutputDimension(useThumbnailSize).width ||
         outputSize.height > _maxOutputDimension(useThumbnailSize).height;
   }
