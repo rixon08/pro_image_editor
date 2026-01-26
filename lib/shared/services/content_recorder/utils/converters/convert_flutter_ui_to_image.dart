0692801f35144c82496514e905f24a9b70140abc
Skyshi Digital Indonesia <skyshidigitalindonesia@Skyshis-MacBook-Pro.local>
2026-01-26 13:36:48 +0700

    Update check size width and height

--- lib/shared/services/content_recorder/utils/converters/convert_flutter_ui_to_image.dart
+++ lib/shared/services/content_recorder/utils/converters/convert_flutter_ui_to_image.dart
@@ -7,6 +7,8 @@ import '/plugins/image/src/image/image.dart' as img;
 /// [uiImage] - The image to be converted.
 Future<img.Image> convertFlutterUiToImage(ui.Image uiImage) async {
   final uiBytes = await uiImage.toByteData(format: ui.ImageByteFormat.rawRgba);
+  
+  print('uiBytes : ${uiImage?.width}x${uiImage?.height}');
 
   final image = img.Image.fromBytes(
     width: uiImage.width,
